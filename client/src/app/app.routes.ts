b1da08bbf2c40fb3a6de11e3000f2b8204b34412
deglm006 <deglm006@morris.umn.edu>
2019-03-27 19:51:56 -0500

    Add link to user page #11

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RideListComponent} from "./rides/ride-list.component";
 import {AddRideComponent} from "./rides/add-ride.component";
+import {UserListComponent} from "./users/user-list.component";
 
 // Route Configuration
 export const routes: Routes = [
   {path: '', component: RideListComponent},
   {path: 'rides', component: RideListComponent},
-  {path: 'addride', component: AddRideComponent}
+  {path: 'addride', component: AddRideComponent},
+  {path: 'user', component: UserListComponent}
 
 ];
 
