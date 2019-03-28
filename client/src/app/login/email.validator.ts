60624e1199f7d82ad89f6ade666ef978f9d2e522
deglm006 <deglm006@morris.umn.edu>
2019-03-28 15:56:35 -0500

    Try to create a validator to check whether a given email is in the db or not #9 #8

--- client/src/app/login/email.validator.ts
+++ client/src/app/login/email.validator.ts
@@ -1,16 +1,34 @@
-
+import {environment} from '../../environments/environment';
 import {FormControl} from '@angular/forms';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
+import {User} from '../users/user';
+import {Injectable} from "@angular/core";
+
+@Injectable()
 export class EmailValidator {
+  readonly baseUrl: string = environment.API_URL + 'users';
+  private userUrl: string = this.baseUrl;
+  constructor(private http: HttpClient) {
+  }
+
+
 
 //check database to see if the given email exists
-  static validEmail(fc: FormControl) {
-    // if () {
+
+  // static emailExists(fc: FormControl){
+  //   var result = this.http.get<User>(environment.API_URL+'users' + '?email=' + fc);
+  //   console.log(result);
+  //   return result!="";
+  // }
+
+  static existingEmail(fc: FormControl) {
+    // if (UserListComponent.existingEmail()) {
     //   return ({
     //     existingEmail: true,
     //   });
     // } else {
-    //   return null;
+    //     return({existingEmail: false});
     //
     // }
   }
