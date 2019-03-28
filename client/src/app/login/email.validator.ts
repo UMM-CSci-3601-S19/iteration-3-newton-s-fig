7f8ff0cd73afd97ac2026d3b8d187844abddb746
deglm006 <deglm006@morris.umn.edu>
2019-03-27 22:24:13 -0500

    Create email validator file #9

---
+++ client/src/app/login/email.validator.ts
@@ -0,0 +1,17 @@
+
+import {FormControl} from '@angular/forms';
+
+export class EmailValidator {
+
+//check database to see if the given email exists
+  static validEmail(fc: FormControl) {
+    // if () {
+    //   return ({
+    //     existingEmail: true,
+    //   });
+    // } else {
+    //   return null;
+    //
+    // }
+  }
+}
