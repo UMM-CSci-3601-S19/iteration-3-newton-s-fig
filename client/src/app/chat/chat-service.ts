22f6617b1138f163862c5ad8157d7c92fe4caa35
Avery Koranda <koran023@morris.umn.edu>
2019-03-29 11:42:45 -0500

    Do some exploratory work for connecting to GetStream's API

--- client/src/app/chat/chat-service.ts
+++ client/src/app/chat/chat-service.ts
@@ -8,15 +8,41 @@ import Credentials from '../../../credentials.json';
 import {Observable} from 'rxjs/Observable';
 
 import {environment} from '../../environments/environment';
+import {Ride} from "../rides/ride";
 
 @Injectable()
 export class ChatService {
 
   private API_KEY = (<any>Credentials).API_KEY;
-  private client = new StreamChat(this.API_KEY);
+  private API_KEY_SECRET = (<any>Credentials).API_KEY_SECRET;
+  private chatClient = new StreamChat(this.API_KEY, this.API_KEY_SECRET);
 
 
   constructor(private http: HttpClient) {
+    this.setUser();
+  }
 
+  async setUser() {
+    await this.chatClient.setUser(
+      {
+        id: 'jlahey',
+        name: 'Jim Lahey',
+        image: 'https://i.imgur.com/fR9Jz14.png',
+      },
+      this.chatClient.devToken('jlahey'),
+    );
+  }
+
+  async createRideChannel(ride: Ride) {
+    const channel = this.chatClient.channel('messaging', ride._id, {
+      name: 'Chat for ride going from ' + ride.origin + ' to ' + ride.destination,
+    });
+
+    let state = await channel.watch();
+
+    const text = 'First test message is this. - Yoda';
+    const response = await channel.sendMessage({
+      text,
+    });
   }
 }
