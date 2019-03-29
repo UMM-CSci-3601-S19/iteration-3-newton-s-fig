339b8793129ad629c4d5fdcb217e73924c61fafd
Avery Koranda <koran023@morris.umn.edu>
2019-03-29 11:39:55 -0500

    Add the messaging FormGroup object to class

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {ChatService} from "./chat-service";
 
+import {FormControl, FormGroup} from "@angular/forms";
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
+  providers: []
 })
 export class ChatComponent implements OnInit {
 
   public messageToSend: string;
+  public sendMessageForm = new FormGroup({
+    messageToSend: new FormControl()
+  });
 
   constructor(public chatService: ChatService) { }
 
