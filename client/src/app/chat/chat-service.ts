3f24764b208571edf2db2f1634f3fe3e1bb4bd6b
Avery Koranda <koran023@morris.umn.edu>
2019-03-30 23:07:10 -0500

    Modify approach to use 'getstream' instead of 'stream-chat'
    Note: the addition of `(window as any).global = window;` to the polyfills.ts page is part of a solution for an "UncaughtReference error: global is undefined" error I was getting after importing 'getstream' to chat-service.ts. See comment section of https://github.com/angular/angular-cli/issues/9827.

--- client/src/app/chat/chat-service.ts
+++ client/src/app/chat/chat-service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {StreamChat} from 'stream-chat';
 
 import Credentials from '../../../credentials.json';
 
@@ -9,59 +8,26 @@ import {Observable} from 'rxjs/Observable';
 import {environment} from '../../environments/environment';
 import {Ride} from "../rides/ride";
 import {User} from "../users/user";
+import * as stream from 'getstream';
 
 @Injectable()
 export class ChatService {
   readonly baseUrl: string = environment.API_URL + 'chat';
   private API_KEY = (<any>Credentials).API_KEY;
   private API_KEY_SECRET = (<any>Credentials).API_KEY_SECRET;
-  private chatClient = new StreamChat(this.API_KEY);
-
 
   constructor(private http: HttpClient) {
-    this.setUser();
+    this.connectStream();
   }
 
-   async setUser() {
-     console.log("API_KEY: " + this.API_KEY);
-     console.log("API_KEY_SECRET: " + this.API_KEY_SECRET);
+  async connectStream() {
+    const user = {
+      _id: 'avery'
+    };
 
-     console.log("disabling auth and permission checks");
-     // await this.chatClient.updateAppSettings({ disable_auth_checks: true, });
-
-     console.log("generating a token");
-     const token = this.getToken({ _id: "jlahey"}).subscribe( async (token) => {
-       console.log("token: " + token);
-
-       console.log("setting a user");
-       await this.chatClient.setUser(
-         {
-           id: 'jlahey',
-           name: 'Jim Lahey',
-           image: 'https://i.imgur.com/fR9Jz14.png',
-         },
-         token
-       );
-       console.log("user was set");
-
-       console.log("creating a channel");
-       await this.createRideChannel();
-     }); //this.chatClient.devToken('jlahey');
-  }
-
-  async createRideChannel(ride?: Ride) {
-    const channel = this.chatClient.channel('messaging', 'ride_id_here', {
-      name: 'Chat for ride going from ' + 'ride.origin' + ' to ' + 'ride.destination',
+    this.getToken(user).subscribe( userToken => {
+      const client = stream.connect(this.API_KEY, userToken,"49831");
     });
-
-    let state = await channel.watch();
-
-    const text = 'First test message is this. - Yoda';
-    const response = await channel.sendMessage({
-      text,
-    });
-
-    console.log(response);
   }
 
   getToken(user: Object): Observable<string> {
