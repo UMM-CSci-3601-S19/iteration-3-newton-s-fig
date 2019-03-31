d1d5c0c1ae206e9881ae4139ce19b50f45ebfd07
Avery Koranda <koran023@morris.umn.edu>
2019-03-30 23:00:16 -0500

    Refactor for readability

--- server/src/main/java/umm3601/chat/ChatRequestHandler.java
+++ server/src/main/java/umm3601/chat/ChatRequestHandler.java
@@ -47,9 +47,10 @@ public class ChatRequestHandler {
     Document user = Document.parse(req.body());
 
     try {
-      Client client = Client.builder("h354aemvhp72", "jux8wqyt428348pjuxxzykmac4fwhw278btuxfbvx6xyd39y3mk2atk89dqw3s55").build();
-      Token userToken = client.frontendToken(user.getString("_id"));
-      return userToken;
+      Client client = Client.builder("h354aemvhp72",
+        "jux8wqyt428348pjuxxzykmac4fwhw278btuxfbvx6xyd39y3mk2atk89dqw3s55")
+        .build();
+      return client.frontendToken(user.getString("_id"));
     } catch (MalformedURLException mue) {
       System.err.println("Bad GetStream URL request was made");
     }
