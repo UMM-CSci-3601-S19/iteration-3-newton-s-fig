d0518076845594a9d504fd04ef7a523006e10dc0
deglm006 <deglm006@morris.umn.edu>
2019-04-01 14:10:41 -0500

    Create signup e2e tests and try to write a test to check functionality of signup button

--- client/e2e/signup.e2e-spec.ts
+++ client/e2e/signup.e2e-spec.ts
@@ -16,14 +16,14 @@ browser.driver.controlFlow().execute = function () {
   // If you're tired of it taking long you can remove this call or change the delay
   // to something smaller (even 0).
   origFn.call(browser.driver.controlFlow(), () => {
-    return protractor.promise.delayed(20);
+    return protractor.promise.delayed(2);
   });
 
   return origFn.apply(browser.driver.controlFlow(), args);
 };
 
 
-describe('login page', () => {
+describe('signup page', () => {
   let page: LoginPage;
 
   beforeEach(() => {
@@ -32,41 +32,33 @@ describe('login page', () => {
   });
 
 
-  it("shouldn't login when given a incorrect email",()=>{
-    page.typeAnEmail("notanemail@email.io");
-    expect(page.elementExistsWithClass("login")).toBeTruthy("We should be on login page");
+  it("shouldn't signup when given a incorrect email",()=>{
+    page.typeAnEmail("nonexistingemail@email.io");
+    expect(page.elementExistsWithClass("signup")).toBeTruthy("We should be on signup page");
     page.login();
 
+    expect(page.elementExistsWithClass("signup")).toBeFalsy("We shouldn't be on signup page");
+    expect(page.elementExistsWithClass("route")).toBeTruthy("We should be on rides page");
+  });
+
+  it("shouldn't login when given a existign email",()=>{
+    page.typeAnEmail("lakeishavaughan@email.io");
+    expect(page.elementExistsWithClass("signup")).toBeTruthy("We should be on signup page");
+    page.login();
 
     var EC = protractor.ExpectedConditions;
 // Waits for an alert pops up.
     browser.wait(EC.alertIsPresent(), 2000);
     var popupAlert = browser.switchTo().alert();
     var alertText = popupAlert.getText();
-    expect(alertText).toMatch('The entered email is not associated with any user. Did you enter your email correctly?');
+    expect(alertText).toMatch('The entered email is already taken.');
 
     // Close alert
     popupAlert.dismiss();
 
-    expect(page.elementExistsWithClass("login")).toBeTruthy("We should be on login page");
+    expect(page.elementExistsWithClass("signup")).toBeTruthy("We should still be on signup page");
     expect(page.elementExistsWithClass("route")).toBeFalsy("We shouldn't be on rides page");
   });
 
-  it("should login when given a correct email",()=>{
-    page.typeAnEmail("lakeishavaughan@email.io");
-    expect(page.elementExistsWithClass("login")).toBeTruthy("We should be on login page");
-    page.login();
-    expect(page.elementExistsWithClass("login")).toBeFalsy("We shouldn't be on login page");
-    expect(page.elementExistsWithClass("route")).toBeTruthy("We should be on rides page");
-  });
-
-  it("signup button should bring us to signup page",()=>{
-    browser.executeScript('location.assign("http://"+location.host+"/rides");');
-
-    expect(page.elementExistsWithClass("login")).toBeTruthy("We should be on login page");
-    page.login();
-    expect(page.elementExistsWithClass("login")).toBeFalsy("We shouldn't be on login page");
-    expect(page.elementExistsWithClass("signup")).toBeTruthy("We should be on signup page");
-  });
 
 });
--- client/e2e/signup.po.ts
+++ client/e2e/signup.po.ts
@@ -1,9 +1,9 @@
 import {browser, element, by, promise, ElementFinder} from 'protractor';
 import {Key} from 'selenium-webdriver';
 
-export class UserPage {
+export class LoginPage {
   navigateTo(): promise.Promise<any> {
-    return browser.get('/users');
+    return browser.get('/signup');
   }
 
   // http://www.assertselenium.com/protractor/highlight-elements-during-your-protractor-test-run/
@@ -20,56 +20,18 @@ export class UserPage {
     return browser.executeScript(setStyle, element(byObject).getWebElement(), 'color: red; background-color: yellow;');
   }
 
-  getUserTitle() {
-    const title = element(by.id('user-list-title')).getText();
-    this.highlightElement(by.id('user-list-title'));
 
-    return title;
-  }
-
-  typeInto(field: string, value: string) {
-    const input = element(by.id(field));
+  typeAnEmail(email: string) {
+    const input = element(by.id('EmailField'));
     input.click();
-    input.sendKeys(value);
+    input.sendKeys(email);
   }
 
-  typeAName(name: string) {
-    const input = element(by.id('userName'));
-    input.click();
-    input.sendKeys(name);
-  }
-
-  selectUpKey() {
-    browser.actions().sendKeys(Key.ARROW_UP).perform();
-  }
 
   backspace() {
     browser.actions().sendKeys(Key.BACK_SPACE).perform();
   }
 
-  getCompany(company: string) {
-    const input = element(by.id('userCompany'));
-    input.click();
-    input.sendKeys(company);
-    this.click('submit');
-  }
-
-  getUserByAge() {
-    const input = element(by.id('userName'));
-    input.click();
-    input.sendKeys(Key.TAB);
-  }
-
-  getUniqueUser(email: string) {
-    const user = element(by.id(email)).getText();
-    this.highlightElement(by.id(email));
-
-    return user;
-  }
-
-  getUsers() {
-    return element.all(by.className('users'));
-  }
 
   elementExistsWithId(idOfElement: string): promise.Promise<boolean> {
     if (element(by.id(idOfElement)).isPresent()) {
@@ -80,6 +42,14 @@ export class UserPage {
 
   elementExistsWithCss(cssOfElement: string): promise.Promise<boolean> {
     return element(by.css(cssOfElement)).isPresent();
+  }
+
+  elementExistsWithClass(classOfElement: string): promise.Promise<boolean> {
+    return element(by.className(classOfElement)).isPresent();
+  }
+
+  signup(){
+    this.click("signupButton");
   }
 
   click(idOfButton: string): promise.Promise<void> {
