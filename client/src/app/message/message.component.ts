9e90537a33fde91b4d29a322680c57c2ffeda430
Avery Koranda <koran023@morris.umn.edu>
2019-04-02 21:58:45 -0500

    Add message times, profile pics, & improved message layout

--- client/src/app/message/message.component.ts
+++ client/src/app/message/message.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Message} from "./message";
 import {User} from "../users/user";
+import moment from 'moment';
 
 @Component({
   selector: 'app-message',
@@ -12,12 +13,14 @@ export class MessageComponent implements OnInit {
   @Input() message: Message;
   private loggedInUser: User = JSON.parse(localStorage.user);
   private fromLoggedInUser: boolean;
+  private prettyDate;
 
   constructor() {
   }
 
   ngOnInit() {
     this.fromLoggedInUser = this.message.from.email == this.loggedInUser.email;
+    this.prettyDate = moment(this.message.sent).fromNow();
     console.log("logged in user: " + this.loggedInUser.email);
     console.log("message's user: " + this.message.from.email);
     console.log("fromLoggedInUser for message \"" + this.message.body + "\" = " + this.fromLoggedInUser);
