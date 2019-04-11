f60159808e658d6b58ffc34312f6cf67eaa00367
Avery Koranda <koran023@morris.umn.edu>
2019-04-11 11:19:32 -0500

    Fix tests
    There was a test that was checking for a no longer existent element in the app menu. This should be the last commit for #9.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mongo-Angular-Spark lab';
+  title = 'MoRide';
 
 
   public text: string;
