0434a98db39ce8e936611abc2899648e2786cca5
deglm006 <deglm006@morris.umn.edu>
2019-04-13 17:42:51 -0500

    Start implementing stepper

--- client/src/app/filter/filter.component.ts
+++ client/src/app/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'filter-component',
@@ -6,5 +7,22 @@ import {Component} from '@angular/core';
 })
 
 export class FilterComponent {
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
+  constructor(private _formBuilder: FormBuilder) { }
+
+  todaysDate(): Date {
+    return new Date();
+  }
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      dateCtrl: ['date']
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
 }
