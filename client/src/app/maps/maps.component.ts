05249d3f485c93838c8a9ecdc94ef8dc6f4fb4e4
arons121 <arons121@morris.umn.edu>
2019-04-18 15:11:05 -0500

    Fix map height issue + start working on font fix
    (committed by @koran023)

--- client/src/app/maps/maps.component.ts
+++ client/src/app/maps/maps.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {element} from "protractor";
 
 @Component({
   selector: 'app-maps',
@@ -7,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class MapsComponent implements OnInit {
 
-  @Input() mapHeightVH: string = "30";
+  @Input() height: string = "300px";
   @Input() lat: number = 45.593614;
   @Input() lng: number = -95.890831;
   @Input() zoom: number = 14;
@@ -15,6 +16,37 @@ export class MapsComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    document.getElementById("map").style.height = this.mapHeightVH + "vh";
+
+    var head = document.getElementsByTagName( 'head' )[0];
+
+// Save the original method
+    var insertBefore = head.insertBefore;
+
+// Replace it!
+    head.insertBefore = function( newElement, referenceElement ) {
+
+      if ( newElement.href && newElement.href.indexOf( 'https://fonts.googleapis.com/css?family=Roboto' ) === 0 ) {
+        return null;
+      }
+
+      insertBefore.call( head, newElement, referenceElement );
+    };
+//     console.log("starting to search for font links");
+//     let links = document.getElementsByTagName("link");
+//     for (let link: HTMLLinkElement in links) {
+//       console.log("found a link!");
+//       const href = link.href;
+//       console.log(href);
+//       if (href && href.indexOf('https://fonts.googleapis.com/css?family=Roboto') === 0) {
+//         // link.remove();
+//       }
+//     }
+  }
+
+  setHeight() {
+    let styles = {
+      'height': this.height
+    };
+    return styles;
   }
 }
